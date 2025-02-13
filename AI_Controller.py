92bfed78f202058ebd30744e4a529a442a4d29ed
Nicki Leon Broichhausen <nicki_broichhausen@outlook.com>
2025-02-13 11:37:52 +0100

    rename ai controller file

---
+++ AI_Controller.py
@@ -0,0 +1,22 @@
+
+from stable_baselines3 import SAC
+import numpy as np
+
+import os
+dirname = os.path.dirname(__file__)
+modelname = os.path.join(dirname, 'satellite_attitude_control')
+
+# Load the trained model
+model = SAC.load(modelname)
+
+def get_control(yaw, target, angular_velocity):
+    attitude_error = np.degrees(yaw) - target
+    if attitude_error < -180:
+        attitude_error = 360 + attitude_error 
+    if attitude_error > 180:
+        attitude_error = -(360 - attitude_error)
+
+    action, _states = model.predict([attitude_error, angular_velocity])
+    return action
+
+
